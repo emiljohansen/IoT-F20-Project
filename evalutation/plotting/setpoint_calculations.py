1424116f4a8c5606053997fc5124910ffbf3506e
Frederik Holm <hollm@live.dk>
2020-05-22 12:00:19 +0200

    Setpoint delay

--- evalutation/plotting/setpoint_calculations.py
+++ evalutation/plotting/setpoint_calculations.py
@@ -1,29 +1,37 @@
 import pandas as pd
 
-command_times_setpoint = pd.read_csv('command_times_setpoint.csv')
-logger_times_setpoint = pd.read_csv('logger_times_setpoint.csv')
+command_times_setpoint = pd.read_csv('command_times_setpoint_new.csv')
+logger_times_setpoint = pd.read_csv('logger_times_setpoint_new.csv')
 
 '''
 Find difference in time between a setpoint and when the light level is reached 
 TODO: include last setpoint, so we can find the time for different lengths (e.g. when changing setpoint from 0 to 4, how long does it take? compared to 1 to 2)
 '''
-to_df = pd.DataFrame(data = {'CommandTime' : [], 'DeviceTime' : [], 'LogTime' : [], 'Value' : []})
-last_value = 101
+to_df = pd.DataFrame(data = {'CommandTime' : [], 'DeviceTime' : [], 'LogTime' : [], 'Value' : [], 'FormerValue': []})
+last_value = 0
+reached_setpoint = 0
+not_reached_setpoint = 0
 for index, row in command_times_setpoint.iterrows():
     command_time = row['After']
+    command_time_upperbound = command_time + 20
     command_value = row['Value']
+    former_value = last_value
     if command_value != last_value:
         last_value = command_value
     else:
         continue
-    time_filter = logger_times_setpoint[logger_times_setpoint['LoggerBefore'] >= command_time]
+    time_filter = logger_times_setpoint[(logger_times_setpoint['LoggerBefore'] >= command_time) & (logger_times_setpoint['LoggerBefore'] <= command_time_upperbound)]
+    print(len(time_filter))
     result = time_filter[time_filter['Light'] == command_value]
     if len(result) == 0:
+        not_reached_setpoint += 1
         continue
+    reached_setpoint += 1
     result_value = result.iloc[0]
     log_time = result_value[0]
     device_time = result_value[1]
-    to_df = to_df.append({'CommandTime' : command_time, 'DeviceTime' : device_time, 'LogTime' : log_time, 'Value' : command_value}, ignore_index=True)
+    to_df = to_df.append({'CommandTime' : command_time, 'DeviceTime' : device_time, 'LogTime' : log_time, 'Value' : command_value, 'FormerValue': former_value}, ignore_index=True)
     #print(log_time)
-
+print(not_reached_setpoint) # A single setpoint is never reached, but that is the last one before experiment was terminated
+print(reached_setpoint)
 to_df.to_csv('setpoint_delays.csv', index=False)
