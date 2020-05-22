91075f34f4fd112aaebc2211b5e0d468820dde5b
Frederik Holm <hollm@live.dk>
2020-05-22 11:00:44 +0200

    Intensity plotting finished

--- evalutation/plotting/intensity_calculations.py
+++ evalutation/plotting/intensity_calculations.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-command_intensity_times = pd.read_csv('command_intensity_times.csv')
-logger_times_intensity = pd.read_csv('logger_times_intensity.csv')
+command_intensity_times = pd.read_csv('command_times_intensity_new.csv')
+logger_times_intensity = pd.read_csv('logger_times_intensity_new.csv')
 
 '''
 Find difference in time between a command and when it is actuated
 '''
-to_df = pd.DataFrame(data = {'CommandTime' : [], 'DeviceTime' : [], 'LogTime' : [], 'Value' : []})
+to_df = pd.DataFrame(data = {'CommandTime' : [], 'DeviceTime' : [], 'ValueChanged' : [], 'Value' : [], 'LightChanged' : []})
 last_value = 101
 for index, row in command_intensity_times.iterrows():
     command_time = row['After']
@@ -19,11 +19,19 @@ for index, row in command_intensity_times.iterrows():
     result = time_filter[time_filter['Intensity'] == command_value]
     if len(result) == 0:
         continue
-    # TODO: time when e.g. if intensity = 100, then light > 1
     result_value = result.iloc[0]
+    
+    # TODO: time when e.g. if intensity = 100, then light > 1
+    if command_value == 100:
+        light_changed = time_filter[time_filter['Light'] >= 2]
+    else:
+        light_changed = time_filter[time_filter['Light'] <= 2]
+    light_changed_value = light_changed.iloc[0]
+    
     log_time = result_value[0]
     device_time = result_value[1]
-    to_df = to_df.append({'CommandTime' : command_time, 'DeviceTime' : device_time, 'LogTime' : log_time, 'Value' : command_value}, ignore_index=True)
+    light_changed_at = light_changed_value[0]
+    to_df = to_df.append({'CommandTime' : command_time, 'DeviceTime' : device_time, 'ValueChanged' : log_time, 'Value' : command_value, 'LightChanged' : light_changed_at}, ignore_index=True)
     #print(log_time)
 
 to_df.to_csv('intensity_delays.csv', index=False)
