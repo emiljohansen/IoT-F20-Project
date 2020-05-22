26ff6c7586985bdd37c1ef02e392709c7551febc
Frederik Holm <hollm@live.dk>
2020-05-22 13:46:19 +0200

    Setpoint plotting finished

--- evalutation/plotting/setpoint_calculations.py
+++ evalutation/plotting/setpoint_calculations.py
@@ -8,6 +8,8 @@ Find difference in time between a setpoint and when the light level is reached
 TODO: include last setpoint, so we can find the time for different lengths (e.g. when changing setpoint from 0 to 4, how long does it take? compared to 1 to 2)
 '''
 to_df = pd.DataFrame(data = {'CommandTime' : [], 'DeviceTime' : [], 'LogTime' : [], 'Value' : [], 'FormerValue': []})
+precision_df = pd.DataFrame(data = {'Light' : [], 'Setpoint': [], 'AbsDiff': []}, dtype='int')
+print(precision_df.dtypes)
 last_value = 0
 reached_setpoint = 0
 not_reached_setpoint = 0
@@ -25,13 +27,25 @@ for index, row in command_times_setpoint.iterrows():
     result = time_filter[time_filter['Light'] == command_value]
     if len(result) == 0:
         not_reached_setpoint += 1
+        last_light = time_filter['Light'].iloc[-1]
+        last_intensity = time_filter['Intensity'].iloc[-1]
+        abs_diff = abs(command_value - last_light)
+        precision_df = precision_df.append({'Light' : int(last_light), 'Setpoint': int(command_value), 'AbsDiff': int(abs_diff)}, ignore_index=True)
+        print(precision_df.dtypes)
         continue
     reached_setpoint += 1
     result_value = result.iloc[0]
     log_time = result_value[0]
     device_time = result_value[1]
     to_df = to_df.append({'CommandTime' : command_time, 'DeviceTime' : device_time, 'LogTime' : log_time, 'Value' : command_value, 'FormerValue': former_value}, ignore_index=True)
-    #print(log_time)
-print(not_reached_setpoint) # A single setpoint is never reached, but that is the last one before experiment was terminated
+    
+    #PRECISION
+    last_light = time_filter['Light'].iloc[-1]
+    last_intensity = time_filter['Intensity'].iloc[-1]
+    abs_diff = abs(command_value - last_light)
+    precision_df = precision_df.append({'Light' : int(last_light), 'Setpoint': int(command_value), 'AbsDiff': int(abs_diff)}, ignore_index=True)
+
+print(not_reached_setpoint) # A single setpoint is always never reached, but that is the last one before experiment was terminated
 print(reached_setpoint)
 to_df.to_csv('setpoint_delays.csv', index=False)
+precision_df.to_csv('setpoint_precision.csv', index=False)
