bb4b41e7409fa8f168393262a3a4885e584354e6
Frederik Holm <hollm@live.dk>
2020-05-27 12:29:08 +0200

    update reception calculations

--- evalutation/plotting/reception_frequency.py
+++ evalutation/plotting/reception_frequency.py
@@ -6,3 +6,7 @@ average_reception = raw_data['reception_time'].mean()
 median_reception = raw_data['reception_time'].median()
 print(average_reception)
 print(median_reception)
+variance = raw_data['reception_time'].var()
+print(variance)
+precision = 1/variance
+print(precision)
